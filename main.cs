30d1d85aa0893046bf5dfc6c4615e640c4b5d24c
Daniela Patiño <danielapatino@fluvip.com>
2018-03-26 16:45:10 -0500

    second commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+public class AgendaApplication
+{
+  static public void Main()
+  {
+    string x = System.Console.ReadLine();
+    if (x == "exit") {
+      return;
+    }
+    System.Console.WriteLine(x);
+  } 
+}
